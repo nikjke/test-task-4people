1fabc4157997070d9ec919bab3f7cfd16c912739
Nikita Zlobin <nikjke@mail.ru>
2024-10-02 15:18:15 +0300

    fixed bugs

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,46 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/js/index.js", // Основной JavaScript-файл, откуда начинается сборка
+  entry: "./src/js/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js", // Имя выходного JavaScript-файла
+    filename: "bundle.js",
     clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.scss$/, // Для всех файлов SCSS
+        test: /\.scss$/,
         use: [
-          "style-loader", // Внедряет стили в DOM в виде <style> тега
-          "css-loader", // Преобразует CSS в CommonJS
-          "sass-loader", // Компилирует SCSS в CSS
+          "style-loader",
+          "css-loader",
+          "sass-loader",
         ],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i, // Для изображений
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name][ext]", // Копируем изображения в папку images
+          filename: "images/[name][ext]",
         },
       },
       {
-        test: /\.html$/, // Для обработки HTML-файлов
+        test: /\.html$/,
         use: ["html-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html", // Основной HTML-шаблон
+      template: "./index.html",
       filename: "index.html",
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"), // Указываем папку для статических файлов
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
-    port: 9000, // Порт для локального сервера
+    port: 9000,
   },
 };
